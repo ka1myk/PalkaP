89382943256ed2509adef712cd5de2776a4992d5
Unknown <kalmyk.mp3@gmail.com>
2018-01-07 20:37:20 +0300

    вывел пин приемника в define

--- glushilka.ino
+++ glushilka.ino
@@ -2,13 +2,15 @@
 данный скетч имитирует глушилку
 */
 
+#define txPin 3 //пин передатчика
+
 void setup() { 
-pinMode(3, OUTPUT); 
+pinMode(txPin, OUTPUT); 
 }
 
 void loop() {
-digitalWrite(3, HIGH);
+digitalWrite(txPin, HIGH);
 delayMicroseconds(500);
-digitalWrite(3, LOW);
+digitalWrite(txPin, LOW);
 delayMicroseconds(500);
 }
